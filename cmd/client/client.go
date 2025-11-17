a3fd7ba49feef56fa73f65aa2786bb46a659f3d0
Aditya Shukla <Shadowshukla76@gmail.com>
2025-11-18 02:06:39 +0530

    added another client

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,17 +5,31 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+func toRESP(input string) string {
+	parts := strings.Fields(input)
+	resp := fmt.Sprintf("*%d\r\n", len(parts))
+
+	for _, p := range parts {
+		resp += fmt.Sprintf("$%d\r\n%s\r\n", len(p), p)
+	}
+
+	return resp
+}
 
 func main() {
 	conn, _ := net.Dial("tcp", "localhost:6380")
+	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("> ")
 		cmd, _ := reader.ReadString('\n')
+		cmd = strings.TrimSpace(cmd)
 
-		conn.Write([]byte(cmd))
+		resp := toRESP(cmd)
+		conn.Write([]byte(resp))
 
 		reply := make([]byte, 1024)
 		n, _ := conn.Read(reply)
