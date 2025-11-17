4318751e625e0b30975179b289130e23e0f2fa47
Aditya Shukla <Shadowshukla76@gmail.com>
2025-11-17 10:05:29 +0530

    added set command

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -1,11 +1,32 @@
 package handlers
 
-import "godis/internals/resp"
+import (
+	"godis/internals/resp"
+	"sync"
+)
+
+var SETs = map[string]string{}
+var SETsMS = sync.RWMutex{}
 
 func ping(args []resp.Value) resp.Value {
 	return resp.Value{
 		Typ: "string",
 		Str: "PONG",
+	}
+}
+
+func SET(args []resp.Value) resp.Value {
+	if len(args) != 2 {
+		return resp.Value{Typ: "error", Str: "ERR wrong number of arguments for 'set' command"}
+	}
+	key := args[0].Bulk
+	value := args[1].Bulk
+	SETsMS.Lock()
+	SETs[key] = value
+	SETsMS.Unlock()
+	return resp.Value{
+		Typ: "string",
+		Str: "OK",
 	}
 }
 
